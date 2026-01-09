b37bd296c072f2f268d215390f42006539fcd8b9
Wolfgang <github@linux-dude.de>
2026-01-09 22:18:48 +0100

    refactor: show persistent status message after import and export
    Signed-off-by: Wolfgang <github@linux-dude.de>

--- src/components/modals/AppSettingsDialog.vue
+++ src/components/modals/AppSettingsDialog.vue
@@ -74,6 +74,15 @@
 						</template>
 					</NcButton>
 				</div>
+				<NcNoteCard v-if="loading" type="info">
+					<template #icon>
+						<NcLoadingIcon size="20" />
+					</template>
+					<template #default>
+						<h1>{{ t('news', 'Importing feeds') }}...{{ t('news', 'Please wait') }}</h1>
+					</template>
+				</NcNoteCard>
+				<NcNoteCard v-else-if="uploadOpmlStatusMessage" :type="uploadOpmlStatusType" :text="uploadOpmlStatusMessage" />
 			</NcFormGroup>
 			<NcFormGroup :label="t('news', 'Articles (JSON)')">
 				<div class="button-container">
@@ -101,6 +110,7 @@
 						</template>
 					</NcButton>
 				</div>
+				<NcNoteCard v-if="uploadArticlesStatusMessage" :type="uploadArticlesStatusType" :text="uploadArticlesStatusMessage" />
 			</NcFormGroup>
 		</NcAppSettingsSection>
 
@@ -205,6 +215,8 @@ import NcFormGroup from '@nextcloud/vue/components/NcFormGroup'
 import NcHotkey from '@nextcloud/vue/components/NcHotkey'
 import NcHotkeyList from '@nextcloud/vue/components/NcHotkeyList'
 import NcKbd from '@nextcloud/vue/components/NcKbd'
+import NcLoadingIcon from '@nextcloud/vue/components/NcLoadingIcon'
+import NcNoteCard from '@nextcloud/vue/components/NcNoteCard'
 import NcRadioGroup from '@nextcloud/vue/components/NcRadioGroup'
 import NcRadioGroupButton from '@nextcloud/vue/components/NcRadioGroupButton'
 import DownloadIcon from 'vue-material-design-icons/Download.vue'
@@ -225,6 +237,8 @@ export default defineComponent({
 		NcHotkey,
 		NcHotkeyList,
 		NcKbd,
+		NcLoadingIcon,
+		NcNoteCard,
 		NcRadioGroup,
 		NcRadioGroupButton,
 		DownloadIcon,
@@ -238,8 +252,6 @@ export default defineComponent({
 	data: () => {
 		return {
 			DISPLAY_MODE,
-			uploadStatus: null,
-			selectedFile: null,
 			showSettings: false,
 			displayModeOptions: [
 				{
@@ -347,6 +359,22 @@ export default defineComponent({
 				this.saveSetting('disableRefresh', newValue)
 			},
 		},
+
+		uploadOpmlStatusMessage() {
+			return this.$store.getters.lastOpmlImportMessage?.message
+		},
+
+		uploadOpmlStatusType() {
+			return this.$store.getters.lastOpmlImportMessage?.type
+		},
+
+		uploadArticlesStatusMessage() {
+			return this.$store.getters.lastArticlesImportMessage?.message
+		},
+
+		uploadArticlesStatusType() {
+			return this.$store.getters.lastArticlesImportMessage?.type
+		},
 	},
 
 	mounted() {
@@ -386,18 +414,17 @@ export default defineComponent({
 		},
 
 		async importOpml(event) {
+			let result
 			const file = event.target.files[0]
-			if (file) {
-				this.selectedFile = file
-			} else {
-				showError(t('news', 'Please select a valid OPML file'))
+			if (!file || !file.name.endsWith('.opml')) {
+				result = { type: 'error', message: t('news', 'Please select a valid OPML file') }
+				this.$store.commit(MUTATIONS.SET_OPML_IMPORT_MESSAGE, { value: result })
 				return
 			}
 
 			this.$store.commit(MUTATIONS.SET_LOADING, { value: true })
-			this.showFeedSettings = true
 			const formData = new FormData()
-			formData.append('file', this.selectedFile)
+			formData.append('file', file)
 
 			try {
 				const response = await fetch(generateUrl('/apps/news/import/opml'), {
@@ -408,13 +435,14 @@ export default defineComponent({
 				if (response.ok) {
 					const data = await response.json()
 					if (data.status === 'ok') {
-						showSuccess(t('news', 'File successfully uploaded'))
+						result = { type: 'success', message: t('news', 'File successfully uploaded') }
 					} else {
-						showError(data.message, { timeout: -1 })
+						result = { type: 'warning', message: data.message }
 					}
 				} else {
-					showError(t('news', 'Error uploading the opml file'))
+					result = { type: 'error', message: t('news', 'Error uploading the opml file') }
 				}
+				this.$store.commit(MUTATIONS.SET_OPML_IMPORT_MESSAGE, { value: result })
 			} catch (e) {
 				this.handleResponse({
 					errorMessage: t('news', 'Error connecting to the server'),
@@ -428,6 +456,7 @@ export default defineComponent({
 		},
 
 		async exportOpml() {
+			let result
 			try {
 				const response = await fetch(generateUrl('/apps/news/export/opml'))
 				if (response.ok) {
@@ -438,7 +467,8 @@ export default defineComponent({
 					link.download = 'subscriptions-' + formattedDate + '.opml'
 					link.click()
 				} else {
-					showError(t('news', 'Error retrieving the opml file'))
+					result = { type: 'error', message: t('news', 'Error retrieving the opml file') }
+					this.$store.commit(MUTATIONS.SET_OPML_IMPORT_MESSAGE, { value: result })
 				}
 			} catch (e) {
 				this.handleResponse({
@@ -449,9 +479,11 @@ export default defineComponent({
 		},
 
 		async importArticles(event) {
+			let result
 			const file = event.target.files[0]
-			if (!file) {
-				showError(t('news', 'Please select a valid json file'))
+			if (!file || !file.name.endsWith('.json')) {
+				result = { type: 'error', message: t('news', 'Please select a valid json file') }
+				this.$store.commit(MUTATIONS.SET_ARTICLES_IMPORT_MESSAGE, { value: result })
 				return
 			}
 
@@ -468,23 +500,25 @@ export default defineComponent({
 				if (response.status === 200) {
 					const data = await response.data
 					if (data.status === 'ok') {
-						showSuccess(t('news', 'File successfully uploaded'))
-						this.$store.dispatch(ACTIONS.FETCH_FEEDS)
+						result = { type: 'success', message: t('news', 'File successfully uploaded') }
 					} else {
-						showError(data.message, { timeout: -1 })
+						result = { type: 'error', message: data.message }
 					}
 				} else {
-					showError(t('news', 'Error uploading the json file'))
+					result = { type: 'error', message: t('news', 'Error uploading the json file') }
 				}
+				this.$store.commit(MUTATIONS.SET_ARTICLES_IMPORT_MESSAGE, { value: result })
 			} catch (e) {
 				this.handleResponse({
 					errorMessage: t('news', 'Error connecting to the server'),
 					error: e,
 				})
 			}
+			this.$store.dispatch(ACTIONS.FETCH_FEEDS)
 		},
 
 		async exportArticles() {
+			let result
 			try {
 				const response = await axios.get(
 					generateUrl('/apps/news/export/articles'),
@@ -498,7 +532,8 @@ export default defineComponent({
 					link.download = 'articles-' + formattedDate + '.json'
 					link.click()
 				} else {
-					showError(t('news', 'Error retrieving the json file'))
+					result = { type: 'error', message: t('news', 'Error retrieving the json file') }
+					this.$store.commit(MUTATIONS.SET_ARTICLES_IMPORT_MESSAGE, { value: result })
 				}
 			} catch (e) {
 				this.handleResponse({
